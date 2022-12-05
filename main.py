9891d55013338359e0c196424bf271b56428897c
Omri Gruman <OmriGr1515@gmail.com>
2022-12-05 12:12:06 +0200

    Initial tests: - Crop to focus on road - TH to detect lanes color - Erode to avoid side lanes - Squash lanes to avoid many lines - Hough lines to detect lines

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import cv2
-from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def print_img(image,name="image"):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    plt.title(name)
+    plt.show()
 
 
 def read_frames_from_video(video_path, num_seconds=30):
@@ -25,10 +33,14 @@ def read_frames_from_video(video_path, num_seconds=30):
     print(f"Successfully read {len(frames)} frames from {video_path}")
 
     return frames, fps
-        
 
+last_left = None
+last_right = None
 
 def preprocess_frames(raw_frames):
+    global last_left
+    global last_right
+
     res_frames = raw_frames
     # Ideas:
         # Crop image
@@ -38,6 +50,102 @@ def preprocess_frames(raw_frames):
             # Normalize colors? 
             # Intensify dark color?
             # Contrast?
+    h, w, c = res_frames.shape
+    top = h*65//100
+
+    # crop 
+    cropped = raw_frames[top:, :, :]
+    #print_img(cropped, 'crop')
+
+    # detect white lines
+    filtered_frame = cv2.inRange(cropped, np.array([180, 180, 180]), np.array([250, 250, 250]))
+    #print_img(filtered_frame, 'filter')
+
+    # avoid irrelevant lanes
+    e_kernel = np.ones((3,1))
+    eroded_frame = cv2.erode(filtered_frame, e_kernel)
+    #print_img(eroded_frame, 'erode')
+
+    # stay with minimal lines
+    for row in range(eroded_frame.shape[0]):
+        ids = [[]]
+        for col in range(eroded_frame.shape[1]):
+            if eroded_frame[row, col] > 0:
+                if len(ids[-1]) > 0:
+                    if col - ids[-1][-1] > 10:
+                        ids.append([])
+                ids[-1].append(col)
+        
+        if len(ids[-1]) > 0:
+            for ii in ids:
+                mid = (ii[0] + ii[-1]) // 2
+                eroded_frame[row, ii] = 0
+                eroded_frame[row, mid:mid+1] = 255
+    #print_img(eroded_frame, 'shrink')
+
+    for th in range(10, 3, -1):
+        lines = cv2.HoughLines(eroded_frame, 1, np.pi / 180, th)
+
+        if lines is not None:
+            lines = sorted(lines[:, 0, :], key=lambda line: line[1])
+            lines = list(filter(lambda line: 35 > abs(line[1]*180/np.pi - 90) > 5, lines))   
+
+            # count lines for each side
+            left_lines = list(filter(lambda line: line[1] < (np.pi / 2), lines))
+            num_left = len(left_lines)
+            right_lines = list(filter(lambda line: line[1] > (np.pi / 2), lines))
+            num_right = len(right_lines)
+            
+            # choose best lines
+            if num_left > 5:
+                
+                if last_left is None:
+                    left_line = left_lines[num_left//2]
+                elif len(left_lines) == 0:
+                    left_line = last_left
+                else:
+                    left_line = sorted(left_lines, key=lambda line: abs(line[1] - last_left[1]))[0]
+
+            if num_right > 5:
+                if last_right is None:
+                    right_line = right_lines[num_left//2]
+                elif len(right_lines) == 0:
+                    right_line = last_right
+                else:
+                    right_line = sorted(right_lines, key=lambda line: abs(line[1] - last_right[1]))[0]
+
+                last_left = left_line
+                last_right = right_line
+                break
+
+    left_line = last_left
+    right_line = last_right
+
+    if lines is not None:
+        all_lines = cropped.copy()
+        for rho, theta in lines:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 2000 * (-b))
+            y1 = int(y0 + 2000 * (a))
+            x2 = int(x0 - 2000 * (-b))
+            y2 = int(y0 - 2000 * (a))
+            all_lines = cv2.line(all_lines, (x1, y1), (x2, y2), (0, 0, 255), thickness=1)
+        #print_img(all_lines, f'th:{th}, lines:{len(lines)}')
+
+    for rho, theta in [left_line, right_line]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 2000 * (-b))
+        y1 = int(y0 + 2000 * (a))
+        x2 = int(x0 - 2000 * (-b))
+        y2 = int(y0 - 2000 * (a))
+        cropped = cv2.line(cropped, (x1, y1), (x2, y2), (0, 0, 255), thickness=1)
+    #print_img(cropped, f'final')
 
     return res_frames
 
@@ -59,7 +167,7 @@ def detect_lanes(preprocessed_frames):
 
 
 def save_frames_to_video(frames, fps):
-    video_path = fr'videos\output\lanes.mp4'
+    video_path = 'lanes.mp4'
     codec = cv2.VideoWriter_fourcc(*"mp4v")
     h, w = frames[0].shape[:2]
     color = True
@@ -75,10 +183,10 @@ def save_frames_to_video(frames, fps):
 
 if __name__ == '__main__':
 
-    input_video_path = r'videos\input\video1.mp4'    
+    input_video_path = 'video1.mp4'    
     raw_frames, fps = read_frames_from_video(input_video_path)
 
-    preprocessed_frames = preprocess_frames(raw_frames)
+    preprocessed_frames = [preprocess_frames(frame) for frame in raw_frames]
     final_frames = detect_lanes(preprocessed_frames)
 
     save_frames_to_video(final_frames, fps)
